1eff43f65346f4a8d285c427f53762ddf04aac42
talllyes <talllyes@gmail.com>
2016-11-13 20:34:08 +0800

    gogo

---
+++ Config_Demo/Dome.js
@@ -0,0 +1,9 @@
+﻿$(function () {
+    $(".demoBodyLeft ul li a").on("click", function () {
+        $(".demoBodyLeft a").removeClass("active");
+        $(this).addClass("active");
+    });
+    var URL = window.location;
+    $(".demoBodyLeft").find('a[href="' + URL.hash + '"]').addClass("active");
+
+});

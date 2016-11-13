eb211de1b69aa5f9c3746a3d4e26b024794fb07d
talllyes <talllyes@gmail.com>
2016-11-13 20:04:24 +0800

    第一次

---
+++ PickRole/PickRoleRoute.js
@@ -0,0 +1,10 @@
+﻿app.config(function ($routeProvider, $locationProvider) {
+    $routeProvider
+     .when('/Role', {
+         templateUrl: 'Role/Role.html',
+         controller: 'Role as Role'
+     })
+    .otherwise({
+        redirectTo: '/Role'
+    });
+});

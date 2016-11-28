c94c9fd512711784df931ea922372403a2e2e8a6
talllyes <talllyes@gmail.com>
2016-11-28 13:47:22 +0800

    aa

--- Com_PickRole/index/Index.js
+++ Com_PickRole/index/Index.js
@@ -1,6 +1,7 @@
 ﻿app.controller('Index', function ($rootScope, $scope, $http, $routeParams) {
     var Index = this;
     Index.sence = [];
+    Index.start = false;
     Index.today = new Date;
     $("#change1").loadchange("on", 1000);
     function getScene() {
@@ -10,6 +11,12 @@
         }).then(function successCallback(response) {
             if (response.data != "無效的指令") {
                 Index.sence = response.data;
+
+                angular.forEach(Index.sence, function (sence, key) {
+                    if (sence.Start == true) {
+                        Index.start = true;
+                    }
+                });
                 $("#change1").loadchange("off")
             } else {
                 alert("連線失敗，免費空間不穩定請重整或稍後再試。");
